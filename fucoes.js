bf6b3f930c9041a450c119a6376442ffae770298
marilinhas <93530906+marilinhas@users.noreply.github.com>
2022-04-05 02:00:19 -0300

    first commit

---
+++ fucoes.js
@@ -0,0 +1,13 @@
+
+
+const getValueInput = () => {
+    var name = document.getElementById("name").value;
+    var date = document.getElementById("date").value;
+    var email = document.getElementById("email").value;
+    var number = document.getElementById("number").value;
+
+document.getElementById("nameValue").innerHTML = name; 
+document.getElementById("dateValue").innerHTML = date;
+document.getElementById("emailValue").innerHTML = email;
+document.getElementById("numberValue").innerHTML = number;   
+}  
